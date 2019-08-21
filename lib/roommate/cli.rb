8184b409b6f8ed9b4a1006198b63d1dc24066640
Tom White <eugene.white@gmail.com>
2019-08-21 00:12:01 +0000

    first commit

---
+++ lib/roommate/cli.rb
@@ -0,0 +1,8 @@
+require "roommate/cli/version"
+
+module Roommate
+  module Cli
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/roommate/cli/version.rb
@@ -0,0 +1,5 @@
+module Roommate
+  module Cli
+    VERSION = "0.1.0"
+  end
+end

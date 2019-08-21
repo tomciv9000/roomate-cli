cbd66392413e27d9d33ed1be973c4633ccdd66cb
Tom White <eugene.white@gmail.com>
2019-08-21 05:57:55 +0000

    new

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,9 @@ Bundler.require
 
 require 'open-uri'
 
+DB = {
+  :connection => SQLite3::Database.new("db/room-development.sqlite")
+}
+
+
 require_all 'lib'
--- lib/room.rb
+++ lib/room.rb
@@ -1,6 +1,6 @@
 class Room 
   
-  attr_accessor :title, :date_created, :price, :url
+  attr_accessor :id, :title, :date_created, :price, :url
   
   def self.create_from_hash(hash)
     new_from_hash(hash).save
@@ -12,11 +12,55 @@ class Room
     room.date_created = hash[:date_created]
     room.price = hash[:price]
     room.url = hash[:url]
-    binding.pry
+    
     room
   end
   
-  def save
-    
+  def self.new_from_db(row)
+    self.new.tap do |room|
+      room.id = row[0]
+      room.title = row[1]
+      room.date_created = row[2]
+      room.price = row[3]
+      room.url = row[4]
+    end  
   end
+  
+  def save
+    insert
+  end
+
+  def self.all 
+    sql = <<-SQL
+      SELECT * FROM rooms;    
+    SQL
+    
+    rows = DB[:connection].execute(sql)
+    rows.collect do |row|
+      self.new_from_db(row)
+    end
+  end
+  
+  def insert
+    sql = <<-SQL
+      INSERT INTO rooms (title, date_created, price, url) VALUES (?, ?, ?, ?)
+    SQL
+    
+    DB[:connection].execute(sql, self.title, self.date_created, self.price, self.url)
+  end
+  
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS rooms (
+        id INTEGER PRIMARY KEY,
+        title TEXT,
+        date_created DATETIME,
+        price TEXT,
+        url TEXT
+      )
+    SQL
+    
+    DB[:connection].execute(sql)
+  end
+  
 end
